f49319dcf8725c0dd40bde473de6f0daa9d36101
morrss <grishkovea@gmail.com>
2024-10-09 22:41:53 +0300

    сделал таймер

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Memory();
+
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("fdffdfdfd");
+            var a = scanner.nextLine();
+            System.out.println(a);
         }
     }
 }
--- src/main/java/org/example/Memory.java
+++ src/main/java/org/example/Memory.java
@@ -1,4 +1,27 @@
 package org.example;
 
-public class Momory {
+public class Memory {
+    private final byte[] RAM = new byte[1024];
+    private final byte[] HARD_DISK = new byte[1024];
+    private final int PAGE_SIZE = 32;
+    private int myVariable;
+
+    private void startTimer() {
+        Thread incrementThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(5000); // Задержка 5 секунд
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // Восстановление статуса прерывания
+                    break; // Выход из цикла при прерывании
+                }
+                myVariable++; // Увеличение переменной
+                System.out.println("myVariable: " + myVariable); // Вывод текущего значения
+            }
+        });
+        incrementThread.start(); // Запуск потока
+    }
+    public Memory(){
+        startTimer();
+    }
 }
