a8e9d5da99d13dd54e0dc09469e4b66c502fcbfa
morrs <grishkovea@gmail.com>
2024-10-10 00:26:33 +0300

    добавил метод отображения памяти

--- src/main/java/org/example/Memory.java
+++ src/main/java/org/example/Memory.java
@@ -1,10 +1,23 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.stream.IntStream;
+
 public class Memory {
     private final byte[] RAM = new byte[1024];
     private final byte[] HARD_DISK = new byte[1024];
     private final int PAGE_SIZE = 32;
     private int myVariable;
+    private final HashMap<Integer, Integer> MEMORY_USAGE;
+    private final HashMap<Integer, Integer> MEMORY_MANAGER;
+
+    public Memory() {
+        MEMORY_USAGE = new LinkedHashMap<>();
+        MEMORY_MANAGER = new HashMap<>();
+        IntStream.range(0, 64).forEach(index -> MEMORY_MANAGER.put(index, index));
+        startTimer();
+    }
 
     private void startTimer() {
         Thread incrementThread = new Thread(() -> {
@@ -21,7 +34,15 @@ public class Memory {
         });
         incrementThread.start(); // Запуск потока
     }
-    public Memory(){
-        startTimer();
+
+    public void checkMemory() {
+        MEMORY_MANAGER.forEach((index, place_index) -> {
+            if (place_index < 32) {
+                System.out.printf("Страница %d расположена в RAM под индексом %d \n", index, place_index);
+            } else {
+                System.out.printf("Страница %d расположена на HD под индексом %d \n", index, place_index-31);
+            }
+        });
     }
+
 }
